5efac743f79316de7b55165d00882e857366a44b
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-10-27 18:12:28 -0300

    Adicionado atualização dos loops
    While, for, if

--- 8-loops.js
+++ 8-loops.js
@@ -5,7 +5,7 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 18;
+const idadeComprador = 17;
 const estaAcompanhada = false;
 let temPassagemComprada = false;
 const destino = "Rio de Janeiro";
@@ -15,12 +15,29 @@ console.log(listaDeDestinos);
 
 const podeComprar = idadeComprador >= 18  || estaAcompanhada == true;
 let contador = 0;
+let destinoExiste = false;
 
 while(contador<3){
     if(listaDeDestinos[contador] == destino) {
-        console.log("Destino existe");
-    }else{
-        console.log("Destino existe");
+        //console.log("Destino existe");
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
+}
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem");
+}else{
+    console.log("Desculpe tivemos um erro !");
+}
+
+
+for(let cont = 0; cont < 3; cont = cont ++){
+    if(listaDeDestinos[contador] == destino) {
+        //console.log("Destino existe");
+        destinoExiste = true;
+        break;
     }
     contador += 1;
 }
